8dda589302446d6e312d3e3332cc46993b621c4a
Adarsh <adarsh.tiwari8843@gmail.com>
2025-07-31 12:36:47 +0530

    sidebar added in right side

--- mohan-greens-project/src/home.jsx
+++ mohan-greens-project/src/home.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import Navbar from "../src/Components/html/navbar";
-import Sidebar from "../src/Components/html/sidebar";
 
 function Home() {
   return (
     <>
-    <main className="main-layout">
-      <section className="navbar-column">
-        <Navbar />
-      </section>
-      <section className="sidebar-column">
-        <Sidebar />
-      </section>
-    </main>
-    
+      <Navbar/>
     </>
   );
 }
