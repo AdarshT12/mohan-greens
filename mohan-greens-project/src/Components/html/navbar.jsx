31124a3ae2f9f66e9f50cd7b9353b12cb1961030
Adarsh <adarsh.tiwari8843@gmail.com>
2025-07-31 12:38:38 +0530

    changes made

--- mohan-greens-project/src/Components/html/navbar.jsx
+++ mohan-greens-project/src/Components/html/navbar.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import '../css/navbar.css';
 import Logo from "../../assests/mohangrrenslogo.png";
-import icons from "../../assests/money-rupee-circle-line.png";
-import icons from "../../assests/building-line.png";
-import icons from "../../assests/wifi-fill.png";
-import icons from "../../assests/map-pin-2-line.png";
-import icons from "../../assests/download-2-line.png";
 
 
 function Navbar() {
@@ -14,14 +9,7 @@ function Navbar() {
       {/* Left: Logo and Title */}
       <div className="navbar-left">
         <img src={Logo} alt="Mohan Greens" className="logo" />
-        <img src={icons} alt="Home Icon" className="Icon" />
-        <img src={icons} alt="Price Icon" className="Icon" />
-        <img src={icons} alt="Site Icon" className="Icon" />
-        <img src={icons} alt="Aminities Icon" className="Icon" />
-        <img src={icons} alt="Locate Icon" className="Icon" />
-        <img src={icons} alt="download icon" className="Icon" />
         <div className="text-info">
-
         </div>
       </div>
 
