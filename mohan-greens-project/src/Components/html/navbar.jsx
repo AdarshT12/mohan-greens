a3620a1b1227712052f1425f7a73c1a1568bf6ad
Vaishnavi <vaishnavisingh0223@gmail.com>
2025-07-31 12:42:41 +0530

    navbar icons done

--- mohan-greens-project/src/Components/html/navbar.jsx
+++ mohan-greens-project/src/Components/html/navbar.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import '../css/navbar.css';
 import Logo from "../../assests/mohangrrenslogo.png";
-import icons from "../../assests/money-rupee-circle-line.png";
-import icons from "../../assests/building-line.png";
-import icons from "../../assests/wifi-fill.png";
-import icons from "../../assests/map-pin-2-line.png";
-import icons from "../../assests/download-2-line.png";
+import 'remixicon/fonts/remixicon.css';
+
 
 
 function Navbar() {
@@ -13,26 +10,19 @@ function Navbar() {
     <nav className="navbar">
       {/* Left: Logo and Title */}
       <div className="navbar-left">
-        <img src={Logo} alt="Mohan Greens" className="logo" />
-        <img src={icons} alt="Home Icon" className="Icon" />
-        <img src={icons} alt="Price Icon" className="Icon" />
-        <img src={icons} alt="Site Icon" className="Icon" />
-        <img src={icons} alt="Aminities Icon" className="Icon" />
-        <img src={icons} alt="Locate Icon" className="Icon" />
-        <img src={icons} alt="download icon" className="Icon" />
+        <img src={Logo} alt="Mohan Greens" className="logo" />        
         <div className="text-info">
 
         </div>
       </div>
-
       {/* Center: Menu Items */}
       <div className="navbar-center">
-        <div className="nav-item active"><i className="fas fa-home"></i> Home</div>
-        <div className="nav-item"><i className="fas fa-rupee-sign"></i> Price</div> 
-        <div className="nav-item"><i className="fas fa-building"></i> Site Plan</div>
-        <div className="nav-item"><i className="fas fa-wifi"></i> Amenities</div>
-        <div className="nav-item"><i className="fas fa-map-marker-alt"></i> Location</div>
-        <div className="nav-item"><i className="fas fa-download"></i> Brochure</div>
+        <div className="nav-item active"><i class="ri-home-9-line"></i> Home</div>
+        <div className="nav-item"><i class="ri-money-rupee-circle-line"></i>Price</div> 
+        <div className="nav-item"><i class="ri-building-line"></i> Site Plan</div>
+        <div className="nav-item"><i class="ri-wifi-line"></i> Amenities</div>
+        <div className="nav-item"><i class="ri-map-pin-2-line"></i> Location</div>
+        <div className="nav-item"><i class="ri-download-2-line"></i> Brochure</div>
       </div>
 
       {/* Right: Blox logo */}
